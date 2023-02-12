ea7da43fafb48080e8ed2fc975b7a2209647e49c
Nicole <xuan60702@gmail.com>
2023-02-12 21:20:41 +0800

    Create 118_pascals_triangle.py

---
+++ 118_pascals_triangle.py
@@ -0,0 +1,12 @@
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        array=[]
+        for i in range(numRows):
+            row = []
+            for j in range(i+1):
+                if j == 0 or j == i:
+                    row.append(1)
+                else:
+                    row.append(array[i-1][j] + array[i-1][j-1])
+            array.append(row)
+        return array
